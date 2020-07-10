a462be5a9300394bb5c3657e9f6ebed4c50dc67d
Lukec <61413934+LukaPovalej@users.noreply.github.com>
2020-07-10 10:51:09 +0200

    pipes + UI

--- Assets/scripts/BirdController.cs
+++ Assets/scripts/BirdController.cs
@@ -1,6 +1,9 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
 
 public class BirdController : MonoBehaviour
 {
@@ -9,19 +12,30 @@ public class BirdController : MonoBehaviour
     private Rigidbody2D body;
 
     private Vector3 initPosition;
+    
+    private int score = 0;
+
+    public GameObject scoreboard;
+
+    private bool isPaused = false; 
+
+    public Button startButton;
 
     // Start is called before the first frame update
     void Start()
     {
         body = GetComponent<Rigidbody2D>();
         initPosition = transform.position;
+        scoreboard.SetActive(false);
+        Button btn = startButton.GetComponent<Button>();
+		btn.onClick.AddListener(StartGame);
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown("space"))
+        if (Input.GetKeyDown("space") || Input.GetMouseButtonDown(0))
         {
             body.velocity = Vector2.zero;
             body.AddForce(new Vector2(0, flyvelocity));
@@ -39,6 +53,37 @@ public class BirdController : MonoBehaviour
 
     void reset() 
     {
-        transform.position = initPosition;
+        if (ScoreTrack.bestScore < score) 
+        {
+            ScoreTrack.bestScore = score;
+        }
+        Time.timeScale = 0f;
+        scoreboard.SetActive(true);
+        isPaused = true;
+        GameObject.FindWithTag("cscore")
+            .GetComponent<UnityEngine.UI.Text>().text = score + "";
+        GameObject.FindWithTag("bscore")
+            .GetComponent<UnityEngine.UI.Text>().text = ScoreTrack.bestScore + "";
     }
+
+    public void updateScore(int amount)
+    {
+        score += amount;
+        GameObject.FindWithTag("ScoreText")
+            .GetComponent<UnityEngine.UI.Text>().text = score + "";
+    }
+
+    void StartGame()
+    {
+        isPaused = false;
+        Time.timeScale = 1f;
+        transform.position = initPosition;
+        SceneManager.LoadScene( SceneManager.GetActiveScene().name );
+    }
+}
+
+
+public class ScoreTrack
+{
+    public static int bestScore = 0;
 }
--- Assets/scripts/spawner.cs
+++ Assets/scripts/spawner.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class spawner : MonoBehaviour
 {
     public GameObject pipePrefab;
+    float minY = 0.5f;
+    float maxY = 2.5f;
+    float delay = 2f;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,6 +17,12 @@ public class spawner : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        delay -= Time.deltaTime;
+
+        if (delay < 0)
+        {
+            delay = 2f; 
+            Instantiate(pipePrefab, new Vector3(3, Random.Range(minY, maxY), 0), Quaternion.identity);
+        }
     }
 }
